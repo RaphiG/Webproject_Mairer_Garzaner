d6006b35872234a933b076a7183ab911faa6ae70
thmairer <tom123456789090@gmail.com>
2020-01-22 08:49:59 +0100

    First Commit - Project added

---
+++ Webproject_M_R/Webproject_M_R/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Webproject_M_R
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Webproject_M_R/Webproject_M_R/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webproject_M_R.MvcApplication" Language="C#" %>
